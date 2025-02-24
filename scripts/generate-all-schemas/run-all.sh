02d19e70ef316cc935bc9d1c70ba41f14c49a1fe
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2025-02-24 16:52:45 +0100

    feat: Add all previous releases

---
+++ scripts/generate-all-schemas/run-all.sh
@@ -0,0 +1,2 @@
+echo ./process-releases.sh --repo oslokommune/golden-path-boilerplate --boilerplate SOMEWHERE/golden-path-boilerplate/boilerplate
+./process-releases.sh --repo oslokommune/golden-path-boilerplate --boilerplate SOMEWHERE/golden-path-boilerplate/boilerplate
